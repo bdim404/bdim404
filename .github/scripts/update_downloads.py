b5eee4825f65bef69f2c015996e584f7f8bf6e4a
bdim <i@bdim.moe>
2025-12-30 17:01:55 +0800

    Update badge in README to reflect Dify plugin downloads

--- .github/scripts/update_downloads.py
+++ .github/scripts/update_downloads.py
@@ -21,18 +21,20 @@ def main():
     with open('README.md', 'r') as f:
         content = f.read()
 
-    badge_url = f'https://img.shields.io/badge/dify_downloads-{total}-blue'
-    badge_html = f'<img src="{badge_url}" alt="Dify Downloads" />'
+    logo_svg = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMTYgMzJDMjQuODM2NiAzMiAzMiAyNC44MzY2IDMyIDE2QzMyIDcuMTYzNDQgMjQuODM2NiAwIDE2IDBDNy4xNjM0NCAwIDAgNy4xNjM0NCAwIDE2QzAgMjQuODM2NiA3LjE2MzQ0IDMyIDE2IDMyWiIgZmlsbD0idXJsKCNwYWludDBfbGluZWFyKSIvPjxkZWZzPjxsaW5lYXJHcmFkaWVudCBpZD0icGFpbnQwX2xpbmVhciIgeDE9IjE2IiB5MT0iMCIgeDI9IjE2IiB5Mj0iMzIiIGdyYWRpZW50VW5pdHM9InVzZXJTcGFjZU9uVXNlIj48c3RvcCBzdG9wLWNvbG9yPSIjMDA2MEZGIi8+PHN0b3Agb2Zmc2V0PSIxIiBzdG9wLWNvbG9yPSIjMDAzM0ZGIi8+PC9saW5lYXJHcmFkaWVudD48L2RlZnM+PC9zdmc+'
+    badge_url = f'https://img.shields.io/badge/dify_plugin-{total}-0033ff?style=flat&logo={logo_svg}&logoColor=white'
+    badge_html = f'<img src="{badge_url}" alt="Dify Plugin Downloads" />'
 
-    pattern = r'<img src="https://img\.shields\.io/badge/dify_downloads-\d+-blue" alt="Dify Downloads" />'
+    pattern = r'<img src="https://img\.shields\.io/badge/dify_plugin-\d+-[^"]*" alt="Dify Plugin Downloads" />'
 
     if re.search(pattern, content):
         new_content = re.sub(pattern, badge_html, content)
     else:
-        komarev_line = '<img src="https://komarev.com/ghpvc/?username=bdim404&abbreviated=true" alt="Profile Views" />'
-        new_content = content.replace(
-            komarev_line,
-            f'{komarev_line}\n{badge_html}'
+        komarev_pattern = r'(<img src="https://komarev\.com/ghpvc/\?username=bdim404&abbreviated=true" alt="Profile Views" />)'
+        new_content = re.sub(
+            komarev_pattern,
+            r'\1 ' + badge_html,
+            content
         )
 
     if new_content != content:
--- README.md
+++ README.md
@@ -9,8 +9,7 @@
 ---
 ## About Me
 
-<img src="https://komarev.com/ghpvc/?username=bdim404&abbreviated=true" alt="Profile Views" />
-<img src="https://img.shields.io/badge/dify_downloads-2689-blue" alt="Dify Downloads" />
+<img src="https://komarev.com/ghpvc/?username=bdim404&abbreviated=true" alt="Profile Views" /> <img src="https://img.shields.io/badge/dify_plugin-2689-0033ff?style=flat&logo=data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMTYgMzJDMjQuODM2NiAzMiAzMiAyNC44MzY2IDMyIDE2QzMyIDcuMTYzNDQgMjQuODM2NiAwIDE2IDBDNy4xNjM0NCAwIDAgNy4xNjM0NCAwIDE2QzAgMjQuODM2NiA3LjE2MzQ0IDMyIDE2IDMyWiIgZmlsbD0idXJsKCNwYWludDBfbGluZWFyKSIvPjxkZWZzPjxsaW5lYXJHcmFkaWVudCBpZD0icGFpbnQwX2xpbmVhciIgeDE9IjE2IiB5MT0iMCIgeDI9IjE2IiB5Mj0iMzIiIGdyYWRpZW50VW5pdHM9InVzZXJTcGFjZU9uVXNlIj48c3RvcCBzdG9wLWNvbG9yPSIjMDA2MEZGIi8+PHN0b3Agb2Zmc2V0PSIxIiBzdG9wLWNvbG9yPSIjMDAzM0ZGIi8+PC9saW5lYXJHcmFkaWVudD48L2RlZnM+PC9zdmc+&logoColor=white" alt="Dify Plugin Downloads" />
 
 I’m bdim
 
